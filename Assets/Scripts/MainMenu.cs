946dee1e8237e55449572b27b611815a99864292
Jamie-LeeD <166004984+Jamie-LeeD@users.noreply.github.com>
2025-06-23 03:22:29 +0200

    cloud save

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,19 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
+    public TextMeshProUGUI hightScore;
+
+    private async void Start()
+    {
+        await CloudSaveManager.Instance.LoadHighScore();
+        string temp = "High Score: ";
+        temp = temp + CloudSaveManager.Instance.tempScore;
+        hightScore.text = temp;
+    }
+    public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
     }
--- Assets/Scripts/UnityCloudScripts/CloudSaveManager.cs
+++ Assets/Scripts/UnityCloudScripts/CloudSaveManager.cs
@@ -4,49 +4,63 @@ using UnityEngine;
 using System;
 using System.Threading.Tasks;
 using UnityEditor.ShaderGraph.Serialization;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 
 public class CloudSaveManager : MonoBehaviour
 {
-    //public PlayerProfile tempLoad;
+    public static CloudSaveManager Instance;
 
-    //public async Task SavePlayerData()
+    public int tempScore;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //private async void Start()
     //{
-    //    var data = new Dictionary<string, object>
+    //    await UnityServices.InitializeAsync();
+
+    //    if (!AuthenticationService.Instance.IsSignedIn)
     //    {
-    //        {
-
-    //        }
-    //   };
-
-    //    await CloudSaveService.Instance.Data.Player.SaveAsync(data);
+    //        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            
+    //    }
     //}
+    public async void SaveHighScore(int highScore)
+    {
 
-    //public async Task LoadPlayerData()
-    //{
-    //    string playerNameValue = "DefaultPlayer";
-    //    Color colourTemp = Color.white;
-    //    string colourString = "#ffffff";
+        var data = new Dictionary<string, object>
+            { { "highScore", highScore } };
 
-    //    var data = await CloudSaveService.Instance.Data.Player.LoadAsync(new HashSet<string> { "playerName", "favouriteColor" });
-    //    if (data.TryGetValue("playerName", out var playerName))
-    //    {
-    //        playerNameValue = playerName.Value.GetAs<string>();
-    //        Debug.Log($"Player Name: {playerNameValue}");
-    //    }
-    //    else
-    //    {
-    //        Debug.LogWarning("Player name not found.");
-    //    }
+        await CloudSaveService.Instance.Data.Player.SaveAsync(data);
+    }
 
+    public async Task LoadHighScore()
+    {
 
-    //    if (data.TryGetValue("favouriteColor", out var favouriteColour))
-    //    {
-    //        colourString = favouriteColour.Value.GetAs<string>();
-    //        Debug.Log($"Favourite Color (hex): {colourString}");
+        int highScoreValue = 0;
+
+        var data = await CloudSaveService.Instance.Data.Player.LoadAsync(new HashSet<string> { "highScore" });
+        if (data.TryGetValue("highScore", out var highScore))
+        {
+            highScoreValue = highScore.Value.GetAs<int>();
+        }
+        else
+        {
+            Debug.LogWarning("No score found");
+        }
 
 
-    //    }
 
-    //    tempLoad = new PlayerProfile(playerNameValue, colourString);
-    //}
+        tempScore = highScoreValue;
+    }
 }
